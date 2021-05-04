1ce9588f97b5a3b03f642e7c6df0b7c960a7df80
hosseinfani <hosseinfani@gmail.com>
2021-05-04 15:16:09 -0400

    Batching bug resolved.

--- src/cmn/document.py
+++ src/cmn/document.py
@@ -1,3 +1,7 @@
+import json
+
+from cmn.member import Member
+from cmn.author import Author
 from cmn.team import Team
 class Document(Team):
     def __init__(self, id, authors, title, doc_type, year, venue, references, fos, keywords):
@@ -24,4 +28,81 @@ class Document(Team):
     
     def get_fields(self):
         return self.fields
-    
+
+    def get_skills(self):
+        return self.get_fields()
+
+    @staticmethod
+    def read_data(data_path, topn=None):
+        counter = 0
+        teams = {}
+        all_members = {}
+        input_data = []
+        output_data = []
+
+        with open(data_path, "r", encoding='utf-8') as jf:
+            # Skip the first line
+            jf.readline()
+            while True:
+                # Read line by line to not overload the memory
+                line = jf.readline()
+                if not line or (topn and counter > topn):
+                    break
+
+                jsonline = json.loads(line.lower().lstrip(","))
+
+                # Retrieve the desired attributes
+                id = jsonline['id']
+                title = jsonline['title']
+                year = jsonline['year']
+                type = jsonline['doc_type']
+                venue = jsonline['venue']
+
+                if 'references' in jsonline.keys():
+                    references = jsonline['references']
+                else:
+                    references = []
+
+                if 'fos' in jsonline.keys():
+                    fos = jsonline['fos']
+                else:
+                    continue
+                if 'keywords' not in jsonline.keys():
+                    keywords = []
+                else:
+                    keywords = jsonline['keywords']
+
+                members = []
+                for auth in jsonline['authors']:
+
+                    # Retrieve the desired attributes
+                    member_id = auth['id']
+                    member_name = auth['name']
+
+                    if 'org' in auth.keys():
+                        member_org = auth['org']
+                    else:
+                        member_org = ""
+
+                    if member_id not in all_members.keys():
+                        member = Author(member_id, member_name, member_org)
+                        all_members[member_id] = member
+                    else:
+                        member = all_members[member_id]
+                    members.append(member)
+
+                    # member = Author(member_id, member_name, member_org)
+                    # members.append(member)
+                    # if member_id not in members.keys():
+                    #     members[member_id] = member
+
+                team = Document(id, members, title, year, type, venue, references, fos, keywords)
+                if team.get_uid() not in teams.keys():
+                    teams[team.get_uid()] = team
+
+                input_data.append(team.get_skills())
+                output_data.append(team.get_members_names())
+
+                counter += 1
+
+        return all_members, teams, input_data, output_data
