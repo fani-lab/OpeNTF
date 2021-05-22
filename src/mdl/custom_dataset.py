1fa12eb7e28851bec09969408d8dc4eb106084d8
Arman Dashti <vaghehdashti@gmail.com>
2021-05-21 20:24:33 -0400

    Simple NN done (probably :D)

--- src/mdl/custom_dataset.py
+++ src/mdl/custom_dataset.py
@@ -8,15 +8,7 @@ class TFDataset(Dataset):
         self.output = output_matrix
     
     def __len__(self):
-        print(self.input.shape[0])
         return self.input.shape[0]
 
     def __getitem__(self, index):
-        # x = torch.tensor(self.input[index])
-        # y = torch.tensor(self.output[index])
-        # return (x, y)
-        print(index)
-        # print(self.input[index].shape)
-        # print(self.output[index].shape)
-
         return torch.as_tensor(self.input[index].toarray()).float(), torch.as_tensor(self.output[index].toarray())
