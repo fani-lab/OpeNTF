03d25cf42aff52f2b06177f04ac167d154fa0aa4
hosseinfani <hosseinfani@gmail.com>
2021-06-30 20:14:10 -0400

    r2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 from cmn.team import Team
-from cmn.document import Document
+from cmn.publication import Publication
 from dal.data_utils import *
 import dnn
 import nmt
@@ -8,14 +8,14 @@ import sgns
 
 sys.path.extend(['../cmn'])
 # raw_data_path = "../data/raw/dblp.v12.json"
-raw_data_path = "../data/raw/named_toy_dataset.json"
+raw_data_path = "../data/raw/named_toy.json"
 
-all_members, teams, input_data, output_data = Document.read_data(raw_data_path, topn=None)
+all_members, teams = Publication.read_data(raw_data_path, topn=None)
 index_to_member, member_to_index = Team.build_index_members(all_members)
 index_to_skill, skill_to_index = Team.build_index_skills(teams)
-splits = create_evaluation_splits(len(teams), 5)
+splits = create_evaluation_splits(len(teams), 2)
 
-dnn.main(splits, teams, skill_to_index, member_to_index, index_to_skill, index_to_member, cmd=['test'])
+dnn.main(splits, teams, skill_to_index, member_to_index, index_to_skill, index_to_member, output='named_toy', cmd=['train', 'test', 'plot', 'eval'])#['train', 'test', 'plot', 'eval']
 
 # nmt.main(splits, input_data, output_data, cmd=['train', 'test', 'eval'])
 
