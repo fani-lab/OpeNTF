fdfc4afdcc27cc015c0959d1bc640af27ad2848c
hosseinfani <hosseinfani@gmail.com>
2021-06-30 20:14:10 -0400

    r2

--- src/cmn/team.py
+++ src/cmn/team.py
@@ -10,18 +10,6 @@ class Team(object):
         Team.count += 1
         self.id = id
         self.members = members
-        self.uid = self.set_uid()
-        
-    # Set a unique id for each team by adding ids of each team member    
-    def set_uid(self):
-        sum_of_ids = 0
-        for mem in self.members:
-            sum_of_ids += mem.get_id()
-        return sum_of_ids
-
-    # Return the unique id of the team
-    def get_uid(self):
-        return self.uid    
     
     # Return a list of members' names    
     def get_members_names(self):
