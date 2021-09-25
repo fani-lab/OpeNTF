91e32de9762e96507d60f8de0f60c771e9c075f6
hosseinfani <hosseinfani@gmail.com>
2021-09-25 01:17:43 -0400

    stats revisited (todo: temporal stats)

--- src/stats.py
+++ src/stats.py
@@ -3,12 +3,13 @@ import sys
 from cmn.publication import Publication
 
 
-# output = 'dblp.v12'
-output = 'toy'
+# output = 'dblp.v12.json'
+output = 'toy.json'
 
-i2m, m2i, i2s, s2i, i2t, t2i, teams = Publication.read_data(data_path=f'../data/raw/{output}.json', output=f'../data/preprocessed/{output}', topn=None)
+# i2m, m2i, i2s, s2i, i2t, t2i, teams = Publication.read_data(data_path=f'../data/raw/{output}.json', output=f'../data/preprocessed/{output}', topn=None)
+#
+# i2m, m2i, i2s, s2i, i2t, t2i, teams = Publication.remove_outliers(output=f'../data/preprocessed/{output}', n=2)
 
-i2m, m2i, i2s, s2i, i2t, t2i, teams = Publication.remove_outliers(output=f'../data/preprocessed/{output}', n=2)
-
-# skill_count_of_pub_count, author_count_of_pub_count, pub_count_of_skills, pub_count_of_authors, pub_count_of_years, skill_count_of_count_of_pub, author_count_of_count_of_pub = Publication.get_stats(teams, raw_data_path)
-stats = Publication.get_stats(teams, f'../data/preprocessed/{output}')
+import pickle
+with open(f'../data/preprocessed/{output}/teamsvecs.pkl', 'rb') as infile:
+    stats = Publication.get_stats(pickle.load(infile), f'../data/preprocessed/{output}', plot=True)
