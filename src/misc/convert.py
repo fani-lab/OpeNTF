4fb4a6a3b3f3a0e56694524db4f3e4e67e84b823
dhwaniPatel <dhwanipatel1911@gmail.com>
2021-10-03 23:34:20 -0400

    fixing convert.py

--- src/misc/convert.py
+++ src/misc/convert.py
@@ -1,12 +1,17 @@
+from os import path
 import pandas as pd 
 
 while True:
-	tsv_file='title_basic.tsv' #replace name of the file with file you want to convert
+	inp_name = input("Enter the name of your .tsv file: \n")
+	if ".tsv" not in inp_name:
+		inp_name = inp_name + ".tsv"	
+	tsv_file = inp_name.lower().replace(" ", "").replace(",", ".")
 	out_name = tsv_file.replace(".tsv", ".csv")
+	filepath = "../../data/raw/" + out_name
 	
-	print("Please wait file is being converted....")
+	print("\nPlease wait file is being converted....")
 	csv_table=pd.read_table(tsv_file,sep='\t', dtype={"isAdult": object, "startYear": object, "isOriginalTitle": object})
-	csv_table.to_csv(out_name,index=False)
-	print("Done. File is converted successfully. \nOff to the next file.")
+	csv_table.to_csv(filepath,index=False)
+	print("Done. File is converted successfully.")
 	break
 
