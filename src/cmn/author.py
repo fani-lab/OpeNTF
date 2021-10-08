8558753d782107108e18f1e66f91b513451a88cf
karan96 <saxenak96@gmail.com>
2021-10-07 20:00:44 -0400

    Update #1

--- src/cmn/author.py
+++ src/cmn/author.py
@@ -1,6 +1,10 @@
-from cmn.member import Member
+from cmn.member import member
 
-class Author(Member):
-    def __init__(self, id, name, org):
-        super().__init__(id, name)
-        self.org = org
+
+class author(member):
+    def __init__(self, id,  inventor_id, loc_id):
+        super().__init__(id, inventor_id)
+        self.loc_id = loc_id
+
+    def get_loc(self):
+        return self.loc_id
