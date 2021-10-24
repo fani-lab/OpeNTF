db05098588b200aaa2555639ca9fcbed6026e103
Arman Dashti <vaghehdashti@gmail.com>
2021-10-24 17:55:12 -0400

    refactoring the code #53

---
+++ src/mdl/nn.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+from torch.nn.functional import leaky_relu
+
+
+class SNN(nn.Module):
+    def __init__(self, input_size, output_size, param):
+        super(SNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, param['d'])
+        self.fc2 = nn.Linear(param['d'], output_size)
+        self.dp = nn.Dropout(0.5)
+        self.initialize_weights()
+
+    def forward(self, x):
+        x = self.dp(leaky_relu(self.fc1(x)))
+        x = self.fc2(x)
+        x = torch.clamp(torch.sigmoid(x), min=1.e-6, max=1. - 1.e-6)
+        return x
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                
+class DNN(nn.Module):
+    def __init__(self, input_size, output_size, param):
+        super(DNN, self).__init__()
+        self.fc1 = nn.Linear(input_size,param['d'])
+        self.hidden_layer = nn.ModuleList([nn.Linear(param['d'],param['d']) for i in range(param['l'])])
+        self.fc2 = nn.Linear(param['d'],output_size)
+        self.dp = nn.Dropout(0.5)
+        self.initialize_weights()
+
+    def forward(self, x):
+        x = leaky_relu(self.fc1(x))
+        for i, l in enumerate(self.hidden_layer):
+            x = leaky_relu(l(x))
+        x = self.dp(x)
+        x = self.fc2(x)
+        x = torch.clamp(torch.sigmoid(x), min=1.e-6, max=1. - 1.e-6)
+        return x
+
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
