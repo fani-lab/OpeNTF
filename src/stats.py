fd02b239037f32c26b7f6549709436f77f29d359
hosseinfani <hosseinfani@gmail.com>
2021-11-19 23:53:17 -0500

    scolar stats on dblp and imdb and two additional distribution (#21 , #20)

--- src/stats.py
+++ src/stats.py
@@ -1,19 +1,15 @@
 import sys
 import pickle
 
-from cmn.publication import Publication
+# from cmn.publication import Publication
 # output = '../data/preprocessed/dblp/dblp.v12.json'
-# output = '../data/preprocessed/dblp/dblp.v12.filtered'
-output = '../data/preprocessed/dblp/toy.json'
-# output = '../data/preprocessed/dblp/toy.json.filtered'
-with open(f'{output}/teamsvecs.pkl', 'rb') as infile:
-    stats = Publication.get_stats(pickle.load(infile), output, plot=True)
+# output = '../data/preprocessed/dblp/toy.dblp.v12.json'
+# with open(f'{output}/teamsvecs.pkl', 'rb') as infile:
+#     stats = Publication.get_stats(pickle.load(infile), output, plot=True)
 
 from cmn.movie import Movie
-# output = '../data/preprocessed/imdb/title.basics.tsv'
-# output = '../data/preprocessed/imdb/title.basics.tsv.filtered'
-output = '../data/preprocessed/imdb/toy.title.basics.tsv'
-# output = '../data/preprocessed/imdb/toy.title.basics.tsv.filtered'
+output = '../data/preprocessed/imdb/title.basics.tsv'
+# output = '../data/preprocessed/imdb/toy.title.basics.tsv'
 with open(f'{output}/teamsvecs.pkl', 'rb') as infile:
     stats = Movie.get_stats(pickle.load(infile), output, plot=True)
 
