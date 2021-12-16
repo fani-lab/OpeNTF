42f80941af46cf14d39a4eb356787d1b2d0bfa41
hosseinfani <hosseinfani@gmail.com>
2021-12-16 15:13:36 -0500

    no message

--- src/eval/metric.py
+++ src/eval/metric.py
@@ -26,15 +26,3 @@ def calculate_auc_roc(Y, Y_):
     fpr, tpr, _ = roc_curve(Y.toarray().ravel(), Y_.ravel())
     return auc, fpr, tpr
 
-def calculate_ranking_metrics(Y, Y_, metrics=['P', 'recall', 'ndcg_cut', 'map_cut']):
-    qrel = dict();
-    run = dict()
-    for i, (y, y_) in enumerate(zip(Y, Y_)):
-        qrel['q' + str(i)] = {'d' + str(idx): 1 for idx in y.nonzero()[1]}
-        run['q' + str(i)] = {'d' + str(j): int(np.round(v * 100)) for j, v in enumerate(y_)}
-        # run['q' + str(i)] = {'d' + str(idx): int(np.round(y_[idx] * 10)) for idx in np.where(y_ > 0.5)[0]}
-    for metric in metrics:
-        met = f'{metric}_2,4,6,8,10'
-        p_evaluator = pytrec_eval.RelevanceEvaluator(qrel, {met})
-        df = pd.DataFrame.from_dict(p_evaluator.evaluate(run))
-        yield metric, df, df.mean(axis=1)
