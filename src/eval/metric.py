902d4d11f4ed1d030f8df75583430f272845e042
Arman Dashti <vaghehdashti@gmail.com>
2021-12-16 17:16:07 -0500

    roc plot inside evaluate func

--- src/eval/metric.py
+++ src/eval/metric.py
@@ -19,7 +19,8 @@ def calculate_metrics(Y, Y_, per_instance=False, metrics={'P_2,5,10', 'recall_2,
     print(f'Evaluating {metrics} ...')
     df = pd.DataFrame.from_dict(pytrec_eval.RelevanceEvaluator(qrel, metrics).evaluate(run))
     print(f'Averaging ...')
-    return df if per_instance else None, df.mean(axis=1).append(pd.Series([aucroc, (fpr, tpr)], index=['aucroc', 'roc']))
+    # return df if per_instance else None, df.mean(axis=1).append(pd.Series([aucroc, (fpr, tpr)], index=['aucroc', 'roc']))
+    return df if per_instance else None, df.mean(axis=1).append(pd.Series([aucroc], index=['aucroc'])), (fpr, tpr) # fpr, tpr is a long string that pandas truncate
 
 def calculate_auc_roc(Y, Y_):
     auc = roc_auc_score(Y.toarray(), Y_, average='micro', multi_class="ovr")
