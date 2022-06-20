7a2ef2cd5ac06a2bc42fc9fa7253b4645105a8f1
hosseinfani <hosseinfani@gmail.com>
2022-06-20 11:52:15 -0400

    adding names of members and some refactors (#117)

--- src/misc/colabstat.py
+++ src/misc/colabstat.py
@@ -7,17 +7,16 @@ import scipy.sparse as sci
 from itertools import combinations
 
 # Returns the pairwise collaborations
-# m_matrix: sparse matrix of teams
-def getPairwiseCollabs(m_matrix):
-    return m_matrix.transpose() @ m_matrix
+# teams_members: sparse matrix of teams['member']
+def get2WayCollabs(teams_members):
+    return teams_members.transpose() @ teams_members
 
 # Returns the top-k in coo_matrix format
-# m_matrix: matrix of pairwise collaborations
+# two_way_collabs: matrix of pairwise collaborations
 # k: top-k
-def getTopK(m_matrix, k):
+def getTopK_2Ways(two_way_collabs, k):
     result = [0] * k
-    upperTri = sci.triu(m_matrix)
-
+    upperTri = sci.triu(two_way_collabs)
     upperTri.setdiag(0)
 
     indexes = sci.find(upperTri)
@@ -26,52 +25,35 @@ def getTopK(m_matrix, k):
     for i in range(0, len(indexes[0]) -1):
         max = i
         for j in range(i+1, len(indexes[0])):
-            if(indexes[2][j] > indexes[2][max]):
-                max = j
+            if(indexes[2][j] > indexes[2][max]): max = j
+
         # Swap the "i" (row) coordinate:
-        temp = indexes[0][i]
-        indexes[0][i] = indexes[0][max]
-        indexes[0][max] = temp
- 
+        indexes[0][max], indexes[0][i] = indexes[0][i], indexes[0][max]
+
         # Swap the "j" (column) coordinate:
-        temp = indexes[1][i]
-        indexes[1][i] = indexes[1][max]
-        indexes[1][max] = temp
+        indexes[1][max], indexes[1][i] = indexes[1][i], indexes[1][max]
 
         # Swap the values
-        temp = indexes[2][i]
-        indexes[2][i] = indexes[2][max]
-        indexes[2][max] = temp
+        indexes[2][max], indexes[2][i] = indexes[2][i], indexes[2][max]
     
-
-    rows = indexes[0][0:k]
-    columns = indexes[1][0:k]
-    data = indexes[2][0:k]
-
-
+    rows, columns, data = indexes[0][0:k], indexes[1][0:k], indexes[2][0:k]
     return sci.coo_matrix((data, (rows, columns)),shape=upperTri.get_shape())
 
-def getTopK_nWays(collabs, k):
-    for i in range(0, len(collabs) - 1):
+def getTopK_nWays(n_way_collabs, k):
+    for i in range(0, len(n_way_collabs) - 1):
         max = i
-        for j in range(i+1, len(collabs)):
-            if(collabs[j][1] > collabs[max][1]):
+        for j in range(i+1, len(n_way_collabs)):
+            if(n_way_collabs[j][1] > n_way_collabs[max][1]):
                 max = j
-        temp = collabs[i]
-        collabs[i] = collabs[max]
-        collabs[max] = temp
-    print(collabs)
-    return collabs[0:k]
+        n_way_collabs[max], n_way_collabs[i] = n_way_collabs[i], n_way_collabs[max]
+    return n_way_collabs[0:k]
         
-
-
 # Plots Results of top-k into a Histogram
 # result: a coo_matrix of the top-k
-def plotTopK(result):
+def plotTopK_2Ways(result):
     # indices[0] for x coordinate
     # indices[1] for y coordinate
     indices = result.nonzero()
-
     data = result.data
     k = len(data) # Gets the k value
 
@@ -80,63 +62,56 @@ def plotTopK(result):
     
     # Form X-Axis Categories:
     xAxis = []
-    for i in range(0,k):
-        xAxis.append("(" + str(indices[0][i]) + "," + str(indices[1][i]) + ")")
+    for i in range(0,k): xAxis.append("(" + str(indices[0][i]) + "," + str(indices[1][i]) + ")")
     
-    # Generates the Graph:
     ax.bar(xAxis,height=data)
-    ax.set_title("Top " + str(k) + " Collaborations")
+    ax.set_title("Top " + str(k) + " 2-Way Collaborations")
     ax.set_ylabel("Frequency")
-    ax.set_xlabel("Pairs in Order of Top-" + str(k))
-
+    ax.set_xlabel("Colabs")
     plt.show()
 
 # Plots Results of top-k into a Histogram
 # result: an array with top-K
-def plotTopK_nWays(result):
+def plotTopK_nWays(result, names=None):
+    if len(result) < 1:
+        print('no data to plot')
+        return
     data = [] # The number of repetitions
     indices = [] # The Coordinates
     for team in result:
         indices.append(team[0])
         data.append(team[1])
 
-    k  = len(data)
+    k = len(data)
     fig = plt.figure()
     ax = fig.add_axes([0.1,0.1,0.7,0.7])
     
     # Form X-Axis Categories:
     xAxis = []
-    for i in range(0,k):
-        xAxis.append(str(indices[i]))
+    for i in range(0,k): xAxis.append(f"({','.join([names[j] for j in indices[i]])})") if names else xAxis.append(str(indices[i]))
     
-    # Generates the Graph:
     ax.bar(xAxis,height=data)
-    ax.set_title("Top " + str(k) + " Collaborations")
+    ax.set_title(f"Top-{str(k)} for {len(indices[0])}-Way Collaborations")
     ax.set_ylabel("Frequency")
-    ax.set_xlabel("Pairs in Order of Top-" + str(k))
-
+    ax.set_xlabel("Collabs")
+    plt.xticks(rotation=90)
     plt.show()
 
-
 # Format: [coordinate, number]
-def nCollabs_combinations(m_matrix, n):      
+def getnWayCollabs(teams_member, n):
     rowIndexes = []
-    for i in range(0, m_matrix.shape[1]):
-        rowIndexes.append(i)
-    
+    for i in range(0, teams_member.shape[1]): rowIndexes.append(i)
 
-    # comb is a list of all the possible combinations
-    # comb (if there are 4 rows) = [0,1,2], [0,1,3], ...
+    # comb is a list of all the possible combinations (if there are 4 rows) = [0,1,2], [0,1,3], ...
     comb = combinations(rowIndexes, n) 
-    # print(list(comb))
 
     # Will record the count for each combination
     # The index for count aligns with it's respective combination
     collabs = []
     for testCase in list(comb):
         tempCount = 0
-        for i in range(0, m_matrix.shape[0]):
-            row = (m_matrix.getrow(i).asformat("array"))[0]
+        for i in range(0, teams_member.shape[0]):
+            row = (teams_member.getrow(i).asformat("array"))[0]
             for j in range(0, n):
                 if(row[testCase[j]] != 1):
                     tempCount -= 1
@@ -146,35 +121,31 @@ def nCollabs_combinations(m_matrix, n):
         if tempCount != 0:
             collabs.append([testCase, tempCount])
 
-    # Prints the results:
     return collabs
 
-def nwayCollabs(m_matrix, n,k):
-    if n==2:
-        m = getPairwiseCollabs(m_matrix)
-        topK = getTopK(m, k)
-        plotTopK(topK)
-    else:
-        m = nCollabs_combinations(m_matrix, n)
-        topK = getTopK_nWays(m, k)
-        plotTopK_nWays(topK)
+def nwayCollabs(teams_members, nway, topk, names):
+    # if nway==2:
+    #     m = get2WayCollabs(teams_members)
+    #     plotTopK_2Ways(getTopK_2Ways(m, topk), names)
+    # else:
+        m = getnWayCollabs(teams_members, nway)
+        plotTopK_nWays(getTopK_nWays(m, topk), names)
 
-# Main Function for Testing:
-def main():  
-    # Test Matrix:
-    A = sci.coo_matrix([[1,1,0,0], [0,0,1,1], [1,1,0,1], [1,1,0,1], [1,0,1,1]])
+def main():
+    # Test Matrix: rows=teams, columns=members
+    A = sci.coo_matrix([[1, 1, 0, 0], [0, 0, 1, 1], [1, 1, 0, 1], [1, 1, 0, 1], [1, 0, 1, 1]])
+    names = None
 
-    with open('data/preprocessed/dblp/toy.dblp.v12.json/teamsvecs.pkl', 'rb') as tfile: 
-        matrix=pickle.load(tfile)
+    # with open('../../data/preprocessed/dblp/toy.dblp.v12.json/teamsvecs.pkl', 'rb') as f: matrix=pickle.load(f)
+    # A = matrix['member']
+    #
+    # with open('../../data/preprocessed/dblp/toy.dblp.v12.json/indexes.pkl', 'rb') as f: indexes=pickle.load(f)
+    # names = indexes['i2c']
 
-    print("---\nTESTING MATRIX:")
     print(A.asformat("array"))
-    nwayCollabs(A, 3, 2)
+    nwayCollabs(A, nway=2, topk=5, names=names)
+    nwayCollabs(A, nway=3, topk=5, names=names)
+    nwayCollabs(A, nway=4, topk=5, names=names)
 
-
-
-
-
-# Uncomment to run main:
-# main()
+main()
 
