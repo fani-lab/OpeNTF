33c71bc113b469ac7733d890963e3b2213ce77a7
hosseinfani <hosseinfani@gmail.com>
2022-09-24 02:05:57 -0400

    no message

---
+++ baseline/KGAT/kgat_test.py
@@ -0,0 +1,6 @@
+from kgat_dataloader import DataLoader
+
+dl = DataLoader('../data/preprocessed/uspt/toy.patent.tsv/teams.pkl', '../data/preprocessed/uspt/toy.patent.tsv/teamsvecs.pkl',
+                '../data/preprocessed/uspt/toy.patent.tsv/indexes.pkl')
+
+
