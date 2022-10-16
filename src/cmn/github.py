9c37e73f690836bb36f6e486ea4dab623731a451
hosseinfani <hosseinfani@gmail.com>
2022-10-16 06:29:42 -0400

    code refactor for github crawler and loader (#167)

--- src/cmn/github.py
+++ src/cmn/github.py
@@ -7,29 +7,32 @@ from src.cmn.developer import Developer
 
 class Repo(Team):
 
-    def __init__(self, idx: int, contributors: list, repo_name: str, languages_and_lines: list, forks_count: int,
-                 stargazers_count: int, created_at: str, pushed_at: str, ncontributions: list, releases: list):
+    def __init__(self, idx: int, contributors: list, name: str, languages_lines: list, nforks: int,
+                 nstargazers: int, created_at: str, pushed_at: str, ncontributions: list, releases: list):
 
-        super().__init__(id=idx, members=contributors, skills=languages_and_lines, datetime=created_at)
-        self.repo_name = repo_name
-        self.forks_count = forks_count
-        self.stargazers_count = stargazers_count
+        super().__init__(id=idx, members=contributors, skills=None, datetime=created_at)
+        self.name = name
+        self.nforks = nforks
+        self.nstargazers = nstargazers
         self.pushed_at = pushed_at
         self.ncontributions = ncontributions
         self.releases = releases
+        self.languages_lines = languages_lines
+        self.skills = self.set_skills()
+
+    def set_skills(self):
+        skills = set()
+        for language in self.languages_lines: skills.add(language[0].lower())
+        return skills
 
     @staticmethod
     def read_data(datapath, output, index, filter, settings):
-
-
         try:
             return super(Repo, Repo).load_data(output, index)
         except (FileNotFoundError, EOFError) as e:
             print(f"Pickles not found! Reading raw data from {datapath} ...")
-
             raw_dataset = pd.read_csv(datapath, converters={'collabs': eval, 'langs': eval, 'rels': eval})
-            dict_of_teams = dict()
-
+            dict_of_teams = dict(); candidates = {}
             try:
                 for idx, row in tqdm(raw_dataset.iterrows(), total=len(raw_dataset)):
                     contributors = row['collabs']
@@ -37,23 +40,23 @@ class Repo(Team):
                     list_of_contributions = list()
 
                     for contributor in contributors:
-                        developer_object = Developer(name=contributor['login'], id=contributor['id'], url=contributor['url'])
-                        list_of_developers.append(developer_object)
+                        if (idname := f"{contributor['id']}_{contributor['login']}") not in candidates:
+                            candidates[idname] = Developer(name=contributor['login'], id=contributor['id'], url=contributor['url'])
+                        list_of_developers.append(candidates[idname])
                         list_of_contributions.append(contributor['contributions'])
 
                     repo_name = row['repo']
-                    languages_and_lines = list(row['langs'].items())
-                    stargazers_count = row['stargazers_count']
-                    forks_count = row['forks_count']
+                    languages_lines = list(row['langs'].items())
+                    nstargazers = row['stargazers_count']
+                    nforks = row['forks_count']
                     created_at = row['created_at']
                     pushed_at = row['pushed_at']
                     ncontributions = row['collabs']
                     releases = row['rels']
 
-                    dict_of_teams[idx] = Repo(idx=idx, contributors=list_of_developers, repo_name=repo_name, releases=releases,
-                                              languages_and_lines=languages_and_lines, stargazers_count=stargazers_count,
-                                              forks_count=forks_count, created_at=created_at, pushed_at=pushed_at, ncontributions=ncontributions)
-            except Exception as e:
-                raise e
+                    dict_of_teams[idx] = Repo(idx=idx, contributors=list_of_developers, name=repo_name, releases=releases,
+                                              languages_lines=languages_lines, nstargazers=nstargazers,
+                                              nforks=nforks, created_at=created_at, pushed_at=pushed_at, ncontributions=ncontributions)
+            except Exception as e: raise e
 
             return super(Repo, Repo).read_data(dict_of_teams, output, filter, settings)
--- src/cmn/github_crawler.py
+++ src/cmn/github_crawler.py
@@ -1,58 +1,65 @@
-import csv, requests, traceback, schedule, time
+import csv, requests, traceback, time, os
+from tqdm import tqdm
+import schedule #conda install --channel=conda-forge schedule
+import logging
+
+from src.cmn.team import Team
+
+class Repo(Team):
+
+    @staticmethod
+    def crawl_repo(writer, reader, GET_header):
+        repo = reader.readline().rstrip('\n')
+        if not repo: return schedule.CancelJob
+        try:
+            print(f'Crawling {repo} ...')
+            collabs = requests.get(f'https://api.github.com/repos/{repo}/contributors', headers=GET_header).json()
+            langs = requests.get(f'https://api.github.com/repos/{repo}/languages', headers=GET_header).json()
+            rels = requests.get(f'https://api.github.com/repos/{repo}/releases', headers=GET_header).json()
+
+            general_repo_info = requests.get(f'https://api.github.com/repos/{repo}', headers=GET_header).json()
+            stargazers_count = general_repo_info['stargazers_count']
+            forks_count = general_repo_info['forks_count']
+            created_at = general_repo_info['created_at']
+            pushed_at = general_repo_info['pushed_at']
+
+            if len(collabs) > 1: writer.writerow({'repo': repo, 'collabs': collabs, 'langs': langs, 'rels': rels, 'pushed_at': pushed_at,
+                                                  'stargazers_count': stargazers_count, 'forks_count': forks_count, 'created_at': created_at})
+        except:
+            print(f'Error occured for repo: {repo}')
+            traceback.print_exc()
+
+    @staticmethod
+    def crawl_github(input, output, access_token, nseconds):
+        '''
+        data.csv is where the output of the data is stored
+
+        repos.csv is the result of a repo list generated using BigQuery
+        Steps to obtain this data:
+        1. Ensure you have a google account
+        2. Open https://console.cloud.google.com/bigquery. Create a new project to enable access to the query.
+        3. Open https://console.cloud.google.com/bigquery?p=bigquery-public-data&d=github_repos to access the editor
+        4. Type the following into the editor
+        SELECT DISTINCT repo.name as repo FROM `githubarchive.day.2020*`
+        WHERE type = 'MemberEvent'
+
+        5. The program above will look at all github MemberEvent data from 2020 (starting on Jan 1). MemberEvents are recorded every time a new person becomes a contributor to a repo.
+        6. Once result is generated, click save result and export to google drive and download the csv file as repos.csv.
+        '''
+        # logging.basicConfig()
+        # schedule_logger = logging.getLogger('schedule')
+        # schedule_logger.setLevel(level=logging.DEBUG)
+
+        header = {'Authorization': "Token " + access_token}
+        with open(input, 'r') as reader, open(output, 'w', newline='') as output_csv:
+            reader.readline() #bypass the header 'repo'
+            w = csv.DictWriter(output_csv, ['repo', 'collabs', 'langs', 'rels', 'stargazers_count', 'forks_count', 'created_at', 'pushed_at'])
+            w.writeheader()
+            job = schedule.every(nseconds).seconds.do(Repo.crawl_repo, writer=w, reader=reader, GET_header=header)  # NOT schedule.every(1).seconds.do(getData, w=w)
+            while True:
+                schedule.run_pending()
+                if not schedule.jobs: return
+                time.sleep(1)
 
 
-def get_data(w):
-    global repo
-    repo = repo.rstrip('\n')
-    try:
-
-        collabs = requests.get(f'https://api.github.com/repos/{repo}/contributors', headers=headers).json()
-        langs = requests.get(f'https://api.github.com/repos/{repo}/languages', headers=headers).json()
-        rels = requests.get(f'https://api.github.com/repos/{repo}/releases', headers=headers).json()
-
-        general_repo_info = requests.get(f'https://api.github.com/repos/{repo}', headers=headers).json()
-        stargazers_count = general_repo_info['stargazers_count']
-        forks_count = general_repo_info['forks_count']
-        created_at = general_repo_info['created_at']
-        pushed_at = general_repo_info['pushed_at']
-
-        if len(collabs) > 1:
-            w.writerow({'repo': repo, 'collabs': collabs, 'langs': langs, 'rels': rels, 'pushed_at': pushed_at,
-                        'stargazers_count': stargazers_count, 'forks_count': forks_count, 'created_at': created_at})
-    except:
-        print(f'Error occured for repo: {repo}')
-        traceback.print_exc()
-    repo = reader.readline()
-
-
-access_token = ''  # Place auth code here
-headers = {'Authorization': "Token " + access_token}
-
-'''
-data.csv is where the output of the data is stored
-
-repos.csv is the result of a repo list generated using BigQuery
-Steps to obtain this data:
-1. Ensure you have a google account
-2. Open https://console.cloud.google.com/bigquery. Create a new project to enable access to the query.
-3. Open https://console.cloud.google.com/bigquery?p=bigquery-public-data&d=github_repos to access the editor
-4. Type the following into the editor
-SELECT DISTINCT repo.name as repo FROM `githubarchive.day.2020*`
-WHERE type = 'MemberEvent'
-
-5. The program above will look at all github MemberEvent data from 2020 (starting on Jan 1). MemberEvents are recorded every time a new person becomes a contributor to a repo.
-6. Once result is generated, click save result and export to google drive and download the csv file as repos.csv.
-'''
-
-counter = 1
-with open('./../../data/raw/gith/data.csv', 'w', newline='') as output_csv, open('./../../data/raw/gith/repos.csv', 'r') as reader:
-    w = csv.DictWriter(output_csv, ['repo', 'collabs', 'langs', 'rels', 'stargazers_count', 'forks_count', 'created_at',
-                                    'pushed_at'])
-    w.writeheader()
-    repo = reader.readline()
-
-    schedule.every(8).seconds.do(get_data, w=w)  # NOT schedule.every(1).seconds.do(getData, w=w)
-
-    while repo != '':
-        schedule.run_pending()
-        time.sleep(1)
+Repo.crawl_github(input='./../../data/raw/gith/toy.repos.csv', output='./../../data/raw/gith/data.csv', access_token='', nseconds=8)
