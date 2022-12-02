a4b59bf230f5d3d8c642ade157c9c091a353a46b
Arman Dashti <vaghehdashti@gmail.com>
2022-12-01 20:22:46 -0500

    fix: updated github loader and developer class to run properly

--- src/cmn/github.py
+++ src/cmn/github.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from tqdm import tqdm
-
 from cmn.team import Team
 from cmn.developer import Developer
 
@@ -20,10 +19,16 @@ class Repo(Team):
         self.releases = releases
         self.languages_lines = languages_lines
         self.skills = self.set_skills()
+        for dev in self.members:
+            dev.teams.add(self.id)
+            dev.skills.update(set(self.skills))
 
     def set_skills(self):
         skills = set()
-        for language in self.languages_lines: skills.add(language[0].lower())
+        top_langs = {'python', 'java', 'c++', 'go', 'javascript', 'typescript', 'php', 'ruby', 'c', 'c#', 'nix', 'scala', 'shell', 'kotlin', 'rust', 'dart', 'swift', 'dm', 'systemverilog', 'lua'}
+        for language in self.languages_lines:
+            if language[0].lower() in top_langs:
+                skills.add(language[0].lower()) 
         return skills
 
     @staticmethod
@@ -32,8 +37,8 @@ class Repo(Team):
             return super(Repo, Repo).load_data(output, index)
         except (FileNotFoundError, EOFError) as e:
             print(f"Pickles not found! Reading raw data from {datapath} ...")
-            raw_dataset = pd.read_csv(datapath, converters={'collabs': eval, 'langs': eval, 'rels': eval})
-            dict_of_teams = dict(); repos = dict(); candidates = {}
+            raw_dataset = pd.read_csv(datapath, converters={'collabs': eval, 'langs': eval, 'rels': eval}, encoding='latin-1')
+            dict_of_teams = dict(); repos = dict(); candidates = dict()
             raw_dataset['created_at'] = pd.to_datetime(raw_dataset['created_at'])
             raw_dataset['year'] = raw_dataset['created_at'].dt.year
             try:
@@ -43,6 +48,7 @@ class Repo(Team):
                     list_of_contributions = list()
 
                     for contributor in contributors:
+                        if isinstance(contributor, str): continue
                         if (idname := f"{contributor['id']}_{contributor['login']}") not in candidates:
                             candidates[idname] = Developer(name=contributor['login'], id=contributor['id'], url=contributor['url'])
                         list_of_developers.append(candidates[idname])
