a1f7ac9bb6b026da13a523872b101c384c881dd2
jamil2388 <ahmed491@uwindsor.ca>
2023-10-19 18:34:58 -0400

    created first gnn classification model, no accuracy though

--- src/mdl/gnn_test.py
+++ src/mdl/gnn_test.py
@@ -1,33 +1,98 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.datasets import KarateClub
-from torch_geometric.data import DataLoader
-from torch_geometric.data import Data
+from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import GCNConv
 
 def load_dataset():
     # Load a graph dataset
-    dataset = KarateClub()
+    # dataset = KarateClub()
+    dataset = Planetoid(root = '~/tmp/Cora', name = 'Cora')
+
+
     return dataset
 
 def explore_dataset(dataset):
-
-    # Access the first graph in the dataset
     data = dataset[0]
-    print(f'Type of the class data : {type(data)}')
+    print(f'Dataset: {dataset}:')
+    print('======================')
 
-    # Print some information about the dataset
-    # print(f"Dataset name: {dataset.name}")
-    print(f"Number of graphs in the dataset: {len(dataset)}")
-    print(f"Number of nodes in the graph: {data.num_nodes}")
-    print(f"Number of edges in the graph: {data.num_edges}")
-    print(f"Node features dimension: {data.num_features}")
-    # print(f"Node labels dimension: {data.num_classes}")
+    print(f'dimension of the data : {len(data)}')
 
-    # Check if the dataset is undirected or directed
-    print(f"Is the graph directed? {data.is_directed()}")
+    print(f'Number of graphs: {len(dataset)}')
+    print(f'Number of features: {dataset.num_features}')
+    print(f'Number of classes: {dataset.num_classes}')
+
+    print(f'Number of nodes: {data.num_nodes}')
+    print(f'Number of edges: {data.num_edges}')
+    print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+    print(f'Number of training nodes: {data.train_mask.sum()}')
+    # print(f'Range of training nodes : {data.train_mask}')
+    print(f'Type of data : {type(data)}')
+    print(f'')
+    print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+    print(f'Contains isolated nodes: {data.has_isolated_nodes()}')
+    print(f'Contains self-loops: {data.has_self_loops()}')
+    print(f'Is undirected: {data.is_undirected()}')
+
+# visualize the data
+def visualize_dataset(data):
+    # G = to_networkx(data, to_undirected=True)
+    # nx.draw(G)
+    pass
+
+def create_model(dataset):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = GCN(dataset).to(device)
+    data = dataset[0].to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.01, weight_decay = 5e4)
+    return model, data, optimizer
+    pass
+
+def train_model(model, data, optimizer):
+    model.train()
+    for epoch in range(200):
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+    return model
+
+
+def evaluate_model(model, data):
+    model.eval()
+    pred = model(data).argmax(dim = 1)
+    correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+    acc = int(correct) / int(data.test_mask.sum())
+    print(f'Accuracy: {acc:.4f}')
+
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, dataset):
+        super().__init__()
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training = self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim = 1)
+
+
+def main():
+    dataset = load_dataset()
+    data = dataset[0]
+    explore_dataset(dataset)
+    # visualize_dataset(data)
+    model, data, optimizer = create_model(dataset)
+    train_model(model, data, optimizer)
+    evaluate_model(model, data)
 
 if __name__ == "__main__":
-    dataset = load_dataset()
-    explore_dataset(dataset)
+    main()
