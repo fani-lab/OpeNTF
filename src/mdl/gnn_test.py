6cbd752b24421a2468738f7ae2af0dc45e1a1cd9
jamil2388 <ahmed491@uwindsor.ca>
2023-10-27 22:38:28 -0400

    gnn_test successful by removing unnecessary functions

--- src/mdl/gnn_test.py
+++ src/mdl/gnn_test.py
@@ -120,7 +120,7 @@ def evaluate_model(model, data):
 
 
 class GCN(torch.nn.Module):
-    def __init__(self, dataset):
+    def __init__(self):
         super().__init__()
         self.conv1 = GCNConv(dataset.num_node_features, 16)
         self.conv2 = GCNConv(16, dataset.num_classes)
@@ -130,14 +130,35 @@ class GCN(torch.nn.Module):
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        x = F.dropout(x, training = self.training)
+        x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
 
-        return F.log_softmax(x, dim = 1)
+        return F.log_softmax(x, dim=1)
+
+def raw_main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = GCN().to(device)
+    data = dataset[0].to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+    model.train()
+    for epoch in range(200):
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+    model.eval()
+    pred = model(data).argmax(dim=1)
+    correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+    acc = int(correct) / int(data.test_mask.sum())
+    print(f'Accuracy: {acc:.4f}')
 
 
 def main():
-
+    dataset = Planetoid(root='/tmp/Cora', name='Cora')
 
 if __name__ == "__main__":
-    main()
+    dataset = Planetoid(root='/tmp/Cora', name='Cora')
+    # main()
+    raw_main()
