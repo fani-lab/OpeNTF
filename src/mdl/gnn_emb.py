551aaa86ec291790c936b979759c99345b8cd533
jamil2388 <ahmed491@uwindsor.ca>
2023-11-08 18:52:04 -0500

    preparing for logging the outputs on sample data

--- src/mdl/gnn_emb.py
+++ src/mdl/gnn_emb.py
@@ -1,4 +1,6 @@
 # import classes from opentf
+import torch_geometric.utils
+
 import param
 import sys,os, json
 from json import JSONEncoder
@@ -10,11 +12,39 @@ from torch_geometric.nn import Node2Vec, MetaPath2Vec
 from torch_geometric.data import Data
 import torch
 import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
 from tqdm import tqdm
 from torch_geometric.datasets import Planetoid
 # import torch.utils.data.dataloader
 
-def create_data(x, edge_index, y = None):
+
+### Hossein Fani ###
+# def create_data(teams, nodetype=['member', 'skill']):
+#     for i in nodetype:
+#         teams[i]# the rows of type i
+#
+#
+#     members, skills, teams = teams[nodetype[i]], teams['skill'], teams['id']
+#     if nodetype == 'expert':
+#         # you need to create a graph of _, graphsize = members.shape
+#         for row in members:
+#             row.data ==> column idx of experts who are connected to each other in this row (team)
+#             create the edges between the idxes
+#
+#     # x = torch.tensor([-1, 0, 1])
+#     # edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+#     # y = torch.tensor([2, 4, 1])
+#
+#     # the data is only for generating the embedding without any target labels
+#     if not y :
+#         data = Data(x = x, edge_index = edge_index)
+#     else:
+#         data = Data(x = x, edge_index = edge_index, y = y)
+#     return data
+
+def create_data_naive(x, edge_index, y = None):
     # x = torch.tensor([-1, 0, 1])
     # edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
     # y = torch.tensor([2, 4, 1])
@@ -37,6 +67,13 @@ def train():
         total_loss += loss.item()
     return total_loss / len(loader)
 
+def plot_graph(team_graph):
+    # make sure that the data given is a Data type object
+    assert type(team_graph) == torch_geometric.data.Data
+
+    G = torch_geometric.utils.to_networkx(team_graph, to_undirected = True)
+    nx.draw(G, with_labels = True)
+
 def plot(x, y):
     mx = max(y)
     mn = min(y)
@@ -46,7 +83,7 @@ def plot(x, y):
     plt.ylabel('Loss')
     plt.ylim(mn - threshold, mx + threshold)
     plt.xlabel('Epochs')
-    plt.xlim(0, 100)
+    plt.xlim(0, len(x))
     plt.plot(x, y)
     plt.legend()
     plt.show()
@@ -54,20 +91,43 @@ def plot(x, y):
 ## main ##
 if __name__ == "__main__":
 
+    # declare necessary variables
+    # the respective heirarchy names needed for the output
+    model_name = 'node2vec'
+    parent_name = 'gnn_emb'
+    dataset_name = 'custom'
+    # the output_path for the testing logs
+    output_path = f'../../output/{dataset_name}/{parent_name}/{model_name}/'
+    os.makedirs(output_path, exist_ok = True)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print()
+    print(f'The current device is {device}')
+    print()
+
     # create a sample data to test
     # this data will have 3 nodes 0, 1 and 2 with 0-1, 1-2 but no 0-2 edge
     # the similarity should be between 0-1, 1-2 but 0-2 should be different from one another
-    data = create_data(torch.tensor([[0], [1], [2]], dtype=torch.float), torch.tensor([[0, 1, 1, 2],[1, 0, 2, 1]], dtype=torch.long)).to(device)
-
+    data = create_data_naive(torch.tensor([[0], [1], [2]], dtype=torch.float), torch.tensor([[0, 1, 1, 2],[1, 0, 2, 1]], dtype=torch.long)).to(device)
     # data = Planetoid('./data/Planetoid', name='Cora')[0]
+
+    # plot the graph data
+    plot_graph(data)
+
+    ### Hossein Fani
+    # teams = {}
+    # teams['skill'] = None
+    # teams['location'] = None
+    # teams['teams'] = None
+    # teams['members'] = scipy.sparse.lil([[1,1,0],[0, 1, 1]])
+    ###
 
     model = Node2Vec(
         data.edge_index,
         embedding_dim = 3,
         walks_per_node = 10,
-        walk_length = 4,
-        context_size = 3,
+        walk_length = 2,
+        context_size = 2,
         p = 1.0,
         q = 1.0,
         num_negative_samples = 1
@@ -89,16 +149,39 @@ if __name__ == "__main__":
     except EOFError:
         print('EOFError')
 
-    losses = []
-    for epoch in range(1, 200):
-        loss = train()
-        losses.append(loss)
-        if(epoch % 5 == 0):
-            print(f'epoch = {epoch : 02d}, loss = {loss : .4f}')
-            print(f'node embeddings : ')
-            # print(f'model.embedding.weight : \n{model.embedding.weight}')
-            # embeddings of first 3 nodes
-            print(f'embeddings from model object (first 3 nodes) : \n{model(torch.tensor([0, 1, 2]))}')
+    # we test for some set of epochs and generate output files based on them
+    num_epochs = [100, 200, 500, 1000, 1500, 2000]
+    for max_epochs in num_epochs:
+        # we keep track of losses in a set of epochs
+        losses = []
 
-    # plotting losses vs epochs
-    plot(list(range(1,200)), losses)
+        # the data file that needs to log our performances
+        output_filename = 'performance_log_'
+        output_filename += f'{max_epochs}_epochs.txt'
+        output_filename_with_path = output_path + output_filename
+        with open(output_filename_with_path, 'w') as outfile:
+            line = f'Graph : \n\n' \
+                   f'x = {data.x}\n' \
+                   f'edge_index = {data.edge_index}\n\n' \
+                   f'---------------------------------\n' \
+                   f'---------------------------------\n' \
+                   f'\nNumber of Epochs : {max_epochs}\n' \
+                   f'---------------------------------\n'
+            for epoch in range(1, max_epochs + 1):
+                loss = train()
+                losses.append(loss)
+                if(epoch % 10 == 0):
+                    print(f'epoch = {epoch : 02d}, loss = {loss : .4f}')
+                    # embeddings of first 3 nodes
+                    print(f'embeddings from model object (first 3 nodes) : \n{np.round(model(torch.tensor([0, 1, 2])).numpy(), 2)}')
+                    # lines to write to file
+                    line += f'Epoch : {epoch}\n\n'
+                    line += f'Node Index__________ Embeddings___________\n'
+                    for i in range(data.x.shape[0]):
+                        line += f'{i} : {np.round(model(i).numpy(), 2)}\n'
+                    line += '\n'
+            # write to file
+            outfile.write(line)
+
+        # plotting losses vs epochs
+        plot(range(1, len(losses) + 1), losses)
