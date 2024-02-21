f62c38047f2918a9c0980bda3d69319a8a390eb4
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2024-02-21 17:45:45 -0500

    Add UMAP24 tutorial website content

---
+++ docs/tutorial/umap24/src/components/Menu.js
@@ -0,0 +1,18 @@
+function Menu() {
+  return (
+    <div className="d-flex flex-column justify-content-center top-menu">
+      <div className="row">
+        <div className="col-8 offset-4">
+          <span className="top-menu-title">
+            <span className="fs-1 text-muted text-uppercase text-center">
+              Team Recommendation
+            </span>
+          </span>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default Menu;
