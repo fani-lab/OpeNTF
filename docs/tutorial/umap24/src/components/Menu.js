9ee423c943bc59172ea1f03fb232644039f83862
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2024-02-21 22:25:22 -0500

    Change header style and hide side menu in mobile

--- docs/tutorial/umap24/src/components/Menu.js
+++ docs/tutorial/umap24/src/components/Menu.js
@@ -2,12 +2,17 @@ function Menu() {
   return (
     <div className="d-flex flex-column justify-content-center top-menu">
       <div className="row">
-        <div className="col-8 offset-4">
+        <div className="col-8 offset-3">
           <span className="top-menu-title">
-            <span className="fs-1 text-muted text-uppercase text-center">
-              Team Recommendation
+            <span className="fs-1 title-text text-uppercase">
+              Collaborative Team Recommendation
             </span>
           </span>
+          <div className="col-8 offset-2">
+            <span className="fw-bold small title-text text-uppercase">
+              For Skilled Users: Objectives, Techniques, and New Perspectives
+            </span>
+          </div>
         </div>
       </div>
       <hr />
