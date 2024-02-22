292245a87b97567fa8621a75cda4321e6170b47f
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2024-02-21 20:14:05 -0500

    Update index.js

--- docs/tutorial/umap24/src/index.js
+++ docs/tutorial/umap24/src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 import Home from './routes/Home';
 
 ReactDOM.render(
-  <HashRouter basename="/tutorial/umap24">
+  <HashRouter basename="/OpeNTF/tutorial/umap24">
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
