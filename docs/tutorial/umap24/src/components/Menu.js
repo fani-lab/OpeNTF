6c12ecdd1cda7012f282f82db75eda350f59dc59
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2024-03-27 21:43:40 -0400

    change title and center alignment

--- docs/tutorial/umap24/src/components/Menu.js
+++ docs/tutorial/umap24/src/components/Menu.js
@@ -2,15 +2,20 @@ function Menu() {
   return (
     <div className="d-flex flex-column justify-content-center top-menu">
       <div className="row">
-        <div className="col-8 offset-3">
-          <span className="top-menu-title">
-            <span className="fs-1 title-text text-uppercase">
-              Collaborative Team Recommendation
-            </span>
-          </span>
-          <div className="col-8 offset-2">
+        <div className="center-block">
+          <div className="text-center">
             <span className="fw-bold small title-text text-uppercase">
-              For Skilled Users: Objectives, Techniques, and New Perspectives
+              UMAP 2024
+            </span>
+          </div>
+          <div className="text-center">
+            <span className="fs-1 title-text text-uppercase">
+              Collaborative Team Recommendation For Skilled Users
+            </span>
+          </div>
+          <div className="text-center">
+            <span className="fw-bold small title-text text-uppercase">
+              Objectives, Techniques, and New Perspectives
             </span>
           </div>
         </div>
