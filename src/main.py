da4f318082122e711ae4302b2a8ad6ff3d8ecddd
jamil2388 <ahmed491@uwindsor.ca>
2024-05-24 15:45:52 -0400

    working on new bnn implementation

--- src/main.py
+++ src/main.py
@@ -97,6 +97,10 @@ def run(data_list, domain_list, fair, filter, future, model_list, output, exp_id
     if 'random' in model_list: models['random'] = Rnd()
     if 'fnn' in model_list: models['fnn'] = Fnn()
     if 'bnn' in model_list: models['bnn'] = Bnn()
+    if 'fbnn' in model_list:
+        from mdl.fbnn import FBnn
+        models['bnn'] = FBnn()
+
     if 'fnn_emb' in model_list: models['fnn_emb'] = Fnn()
     if 'bnn_emb' in model_list: models['bnn_emb'] = Bnn()
     if 'nmt' in model_list: models['nmt'] = Nmt()
