9076e01786acbfee90871e4a9b9384238bf79518
thangk <thangk@uwindsor.ca>
2024-06-11 17:16:05 -0400

    Refactor code for readability and add GPU usage configuration
    - Reformatted code for better readability and consistency.
    - Added environment variable to use the 8th GPU.
    - Updated comments and docstrings for clarity.
    - Fixed minor typos and improved argument parsing in `main.py` and `team2vec/main.py`.
    - Added vocabulary size limit to `nmt_config.yaml`.

--- src/mdl/team2vec/wnn.py
+++ src/mdl/team2vec/wnn.py
@@ -8,13 +8,8 @@ import argparse
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-# teams as documents, members/skills as words
-# doc_list = ['m1 m2 m3','m2 m4'] or
-# doc_list = ['s1 s2', 's3'] or
-# doc_list = ['s1 s2 m1 m2 m3','s3 m2 m4']
-# label_list = ['t1','t2','t3']
-
 from team2vec import Team2Vec
+
 class Wnn(Team2Vec):
     def __init__(self, teamsvecs, indexes, settings, output):
         super().__init__(teamsvecs, indexes, settings, output)
@@ -34,14 +29,22 @@ class Wnn(Team2Vec):
             # End Hossein
             datetime_doc = [f"dt{str(year)}"]
 
-            if   self.settings["embtype"] == 'skill': td = gensim.models.doc2vec.TaggedDocument(skill_doc, [str(int(id[0, 0]))])
-            elif self.settings["embtype"] == 'member': td = gensim.models.doc2vec.TaggedDocument(member_doc, [str(int(id[0, 0]))])
-            elif self.settings["embtype"] == 'joint': td = gensim.models.doc2vec.TaggedDocument(skill_doc + member_doc, [str(int(id[0, 0]))])
-            elif self.settings["embtype"] == 'dt2v': td = gensim.models.doc2vec.TaggedDocument(skill_doc + datetime_doc, [str(int(id[0, 0]))])
+            if self.settings["embtype"] == 'skill': 
+                td = gensim.models.doc2vec.TaggedDocument(skill_doc, [str(int(id[0, 0]))])
+            elif self.settings["embtype"] == 'member': 
+                td = gensim.models.doc2vec.TaggedDocument(member_doc, [str(int(id[0, 0]))])
+            elif self.settings["embtype"] == 'joint': 
+                td = gensim.models.doc2vec.TaggedDocument(skill_doc + member_doc, [str(int(id[0, 0]))])
+            elif self.settings["embtype"] == 'dt2v': 
+                td = gensim.models.doc2vec.TaggedDocument(skill_doc + datetime_doc, [str(int(id[0, 0]))])
+            
             self.data.append(td)
+        
         print(f'#Documents with word type of {self.settings["embtype"]} have created: {len(self.data)}')
         print(f'Saving the {self.settings["embtype"]} documents ...')
-        with open(file, 'wb') as f: pickle.dump(self.data, f)
+        with open(file, 'wb') as f: 
+            pickle.dump(self.data, f)
+        
         return self.data
 
     def train(self):
@@ -54,31 +57,31 @@ class Wnn(Team2Vec):
             print(f"File not found! Learning {output}.mdl embeddings from scratch ...")
 
             self.model = gensim.models.Doc2Vec(dm=self.settings["dm"],
-                                               # training algorithm. If dm=1, ‘distributed memory’ (PV-DM) is used. Otherwise, distributed bag of words (PV-DBOW) is employed.
                                                vector_size=self.settings["embedding_dim"],
                                                window=self.settings["window"],
                                                dbow_words=self.settings["dbow_words"],
-                                               # ({1,0}, optional) – If set to 1 trains word-vectors (in skip-gram fashion) simultaneous with DBOW doc-vector training; If 0, only trains doc-vectors (faster).
                                                min_alpha=0.025,
                                                min_count=0,
                                                seed=0,
                                                workers=multiprocessing.cpu_count())
 
-            if not self.data: self.init()
+            if not self.data: 
+                self.init()
+            
             self.model.build_vocab(self.data)
             for e in tqdm(range(self.settings['max_epochs'])):
-                self.model.train(self.data, total_examples=self.model.corpus_count, epochs=self.model.epochs)
+                self.model.train(self.data, total_examples=self.model.corpus_count, epochs=1)
                 self.model.alpha -= 0.002  # decrease the learning rate
                 self.model.min_alpha = self.model.alpha  # fix the learning rate, no decay
 
             print(f'Saving model for {output} ...')
             self.model.save(f'{self.output}.mdl')
-            # self.model.save_word2vec_format(f'{output}.w2v')
-            # self.model.docvecs.save_word2vec_format(f'{output}.d2v')
             return self.model
-        except Exception as e: raise e
+        except Exception as e: 
+            raise e
 
-    def dv(self): return self.model.docvecs.vectors_docs
+    def dv(self): 
+        return self.model.docvecs.vectors_docs
 
     def infer_d2v(self, words):
         iv = self.model.infer_vector(words)
@@ -86,15 +89,62 @@ class Wnn(Team2Vec):
 
 
 def addargs(parser):
-    embedding = parser.add_argument_group('Team Doc2Vec Embedding')
-    embedding.add_argument('-teamsvecs', type=str, required=True, help='The path to the teamsvecs.pkl and indexes.pkl files; (e.g., ../data/preprocessed/dblp/toy.dblp.v12.json/')
-    embedding.add_argument('-dm', type=int, default=1, help='The training algorithm; (1: distributed memory (default), 0: CBOW')
-    embedding.add_argument('-dbow_words', type=int, default=0, help='Train word-vectors in skip-gram fashion; (0: no (default), 1: yes')
-    embedding.add_argument('-embedding_dim', type=int, default=100, help='Embedding vector dimension; (100 default)')
-    embedding.add_argument('-window', type=int, default=1, help='Coocurrence window; (1 default)')
-    embedding.add_argument('-max_epochs', type=int, default=10, help='Training epoch; (10 default)')
-    embedding.add_argument('-embtype', type=str, default='skill', help="Embedding types; (-embtypes=skill (default); member; joint; )")
-    embedding.add_argument('-output', type=str, required=True, help='Output folder; (e.g., ../data/preprocessed/dblp/toy.dblp.v12.json/')
+    args = [
+        {
+            'flag': '-teamsvecs',
+            'type': str,
+            'required': True,
+            'help': 'The path to the teamsvecs.pkl and indexes.pkl files; (e.g., ../data/preprocessed/dblp/toy.dblp.v12.json/'
+        },
+        {
+            'flag': '-dm',
+            'type': int,
+            'default': 1,
+            'help': 'The training algorithm; (1: distributed memory (default), 0: CBOW'
+        },
+        {
+            'flag': '-dbow_words',
+            'type': int,
+            'default': 0,
+            'help': 'Train word-vectors in skip-gram fashion; (0: no (default), 1: yes'
+        },
+        {
+            'flag': '-embedding_dim',
+            'type': int,
+            'default': 100,
+            'help': 'Embedding vector dimension; (100 default)'
+        },
+        {
+            'flag': '-window',
+            'type': int,
+            'default': 1,
+            'help': 'Coocurrence window; (1 default)'
+        },
+        {
+            'flag': '-max_epochs',
+            'type': int,
+            'default': 10,
+            'help': 'Training epoch; (10 default)'
+        },
+        {
+            'flag': '-embtype',
+            'type': str,
+            'default': 'skill',
+            'help': "Embedding types; (-embtypes=skill (default); member; joint; )"
+        },
+        {
+            'flag': '-output',
+            'type': str,
+            'required': True,
+            'help': 'Output folder; (e.g., ../data/preprocessed/dblp/toy.dblp.v12.json/'
+        }
+    ]
+    
+    parser.add_argument_group('Team Doc2Vec Embedding')
+    for arg in args:
+        flag = arg.pop('flag')
+        parser.add_argument(flag, **arg)
+
 
 def run(teamsvecs_file, indexes_file, settings, output):
     with open(teamsvecs_file, 'rb') as teamsvecs_f, open(indexes_file, 'rb') as indexes_f:
@@ -103,22 +153,6 @@ def run(teamsvecs_file, indexes_file, settings, output):
         t2v.init()
         t2v.train()
 
-        #unit tests :D
-        # if embtype == 'skill': print(t2v.model['s5'])
-        # if embtype == 'member': print(t2v.model['m5'])
-        # if embtype == 'joint':
-        #     print(t2v.model['s5'])
-        #     print(t2v.model['m5'])
-        # print(t2v.model.docvecs[10])#teamid
-
-
-#python -u wnn.py
-# -teamsvecs=../data/preprocessed/dblp/toy.dblp.v12.json/teamsvecs.pkl
-# -embtype=skill
-# -embedding_dim=100
-# -dbow_words=1
-# -output=../data/preprocessed/dblp/toy.dblp.v12.json/
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Team Doc2Vec Embedding')
     addargs(parser)
@@ -126,9 +160,8 @@ if __name__ == "__main__":
     settings = {'dm': args.dm,
                 'dbow_words': args.dbow_words,
                 'embedding_dim': args.embedding_dim,
-                'window': args.windsow,
+                'window': args.window,  # Fixed typo here, Kap:
                 'max_epochs': args.max_epochs,
                 'embtype': args.embtype}
 
     run(f'{args.teamsvecs}teamsvec.pkl', f'{args.teamsvecs}indexes.pkl', settings, args.output)
-
