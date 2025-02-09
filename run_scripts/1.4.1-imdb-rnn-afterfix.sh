3edbb5f24f77a22789fd0e3e0825cf2e096a5c6a
Kap Thang <thangk@uwindsor.ca>
2025-02-09 03:43:56 -0500

    Add run scripts for multiple models and datasets in version 1.4.1
    - Create bash scripts for ConvS2S, RNN, and Transformer models
    - Cover datasets: DBLP, GitHub, IMDB, and USPT
    - Implement consistent run script template with nohup and logging
    - Configure GPU settings and dataset paths for each script
    - Support afterfix model variants and batch size variations

---
+++ run_scripts/1.4.1-imdb-rnn-afterfix.sh
@@ -0,0 +1,8 @@
+models=("rnn-afterfix") 
+
+declare -A dataset_paths
+dataset_paths["toy_dblp"]="../data/preprocessed/dblp/toy.dblp.v12.json"
+dataset_paths["dblp"]="../data/preprocessed/dblp/dblp.v12.json.filtered.mt75.ts3"
+{{-}}dataset_paths["gith"]="../data/preprocessed/gith/gith.data.csv.filtered.mt10.ts3"
+dataset_paths["imdb"]="../data/preprocessed/imdb/imdb.title.basics.tsv.filtered.mt75.ts3"
+dataset_paths["uspt"]="../data/preprocessed/uspt/uspt.patent.tsv.filtered.mt75.ts3" 
