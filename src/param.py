7dc00d69da283690018c7958ada449e719196c81
Kap Thang <thangk@uwindsor.ca>
2025-03-14 02:02:22 -0400

    Add GPU configuration and preprocessing filters to settings
    - Update the settings in `param.py` to include a new GPU configuration option.
    - Comment out existing command options for clarity and future use.
    - Introduce new filters for preprocessing, allowing for more flexible data handling.
    - Add a new dataset option for 'gith' and enhance processing settings for better performance control.

--- src/param.py
+++ src/param.py
@@ -9,6 +9,7 @@ torch.cuda.manual_seed_all(0)
 np.random.seed(0)
 
 settings = {
+    "gpus": "2",
     "model": {
         "baseline": {
             "random": {"b": 128},
@@ -41,10 +42,10 @@ settings = {
             },
         },
         "cmd": [
-            "train",
-            "test",
-            "eval",
-            "fair",
+            # "train",
+            # "test",
+            # "eval",
+            # "fair",
         ],  # 'train', 'test', 'eval', 'plot', 'agg', 'fair'
         "nfolds": 5,
         "train_test_split": 0.85,
@@ -55,15 +56,33 @@ settings = {
             "dblp": {},
             "uspt": {},
             "imdb": {},
+            "gith": {},
         },
         "location_type": "country",  # should be one of 'city', 'state', 'country' and represents the location of members in teams (not the location of teams)
-        "filter": {
-            "min_nteam": 5,
-            "min_team_size": 2,
+
+        # filters that can be applied in first pass of preprocessing
+        "pass1_filters": {
+            "remove_duplicates": False, # True for removing duplicates
+            "remove_empty_skills": False, # True for removing empty skills
+            "remove_empty_members": False, # True for removing empty teams
+            "min_team_size": -1, # -1 for no filtering
+            "max_team_size": -1, # -1 for no filtering
+            "min_skills": -1, # -1 for no filtering
+            "max_skills": -1, # -1 for no filtering
         },
-        "parallel": 1,
-        "ncore": 0,  # <= 0 for all
-        "bucket_size": 1000,
+        "filter": {
+            # filters included in output folder name
+        },
+        # filters that require new rounds of data processing
+        "passn_filters": {
+            "min_nteam": -1, # -1 for no filtering
+        },
+        "processing": {
+            "debug_logs": False, # True for debug logs
+            "nthreads": 0,  # <= 0 for all cores
+            "cpu_batch_size": 25_000, # -1 for domain-specific batch size in their own code in src/cmn/<domain>.py
+            "gpu_batch_size": 500_000, # -1 for domain-specific batch size in their own code in src/cmn/<domain>.py
+        },
     },
     "fair": {
         "np_ratio": None,
@@ -77,5 +96,5 @@ settings = {
         "mode": 0,
         "core": -1,
         "attribute": ["gender", "popularity"],
-    },
+    }
 }
