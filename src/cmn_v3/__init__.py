eb9ca135d3d43af79924a6a580ca5d7771f6792d
Kap Thang <thangk@uwindsor.ca>
2025-03-18 10:55:06 -0400

    Refactor __init__.py to streamline exports by removing unused parameters and ensuring consistent formatting. This change enhances code clarity and maintainability.

--- src/cmn_v3/__init__.py
+++ src/cmn_v3/__init__.py
@@ -17,19 +17,15 @@ from .team import Team, tprint
 from .dblp import Publication
 from .dblp_author import DblpAuthor
 from .dblp_venue import DblpVenue
-from .dblp_params import DBLP_PARAMS
 from .gith import Repository
 from .gith_contributor import GithContributor
-from .gith_params import GITH_PARAMS
 
 __all__ = [
-    'Team',
-    'Publication',  # DBLP
-    'DblpAuthor',   # DBLP-specific author class
-    'DblpVenue',   # DBLP-specific venue class
-    'Repository',   # Gith
-    'GithContributor',  # Gith-specific contributor class
-    'tprint',
-    'DBLP_PARAMS',
-    'GITH_PARAMS'
-] 
+    "Team",
+    "Publication",  # DBLP
+    "DblpAuthor",  # DBLP-specific author class
+    "DblpVenue",  # DBLP-specific venue class
+    "Repository",  # Gith
+    "GithContributor",  # Gith-specific contributor class
+    "tprint",
+]
