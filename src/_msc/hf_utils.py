9f765ef1d978325aafbe3f7c0590c4b6829ad6a7
Gabriel Rueda <gabriel.i.rueda@gmail.com>
2025-05-22 13:16:34 -0400

    Added info for creating key

--- src/_msc/hf_utils.py
+++ src/_msc/hf_utils.py
@@ -1,33 +1,36 @@
-from huggingface_hub import HfApi, hf_hub_download, file_exists
-
+from huggingface_hub import HfApi
 
 '''
 Obtain hf token:
-Go to https://huggingface.co/settings/tokens and create a new token
+1. Go to https://huggingface.co/settings/tokens
+2, Create a new token and got to section "Repositories Permission"
+3. Search for the "datasets/fani-lab/OpeNTF-test" and then check the following permissions:
+    - Read access to contents of selected repos
+    - Write access to contents/settings of selected repos
 '''
 hf_api = HfApi(token="")
 
 
 # Upload as a folder
-hf_api.upload_folder(
-    folder_path="./output/gith/toy.repos.csv",
-    path_in_repo="/output/gith/toy.repos.csv",
-    repo_id="fani-lab/OpeNTF-test",
-    repo_type="dataset"
-)
+# hf_api.upload_folder(
+#     folder_path="./output/gith/toy.repos.csv",
+#     path_in_repo="/output/gith/toy.repos.csv",
+#     repo_id="fani-lab/OpeNTF-test",
+#     repo_type="dataset"
+# )
 
 
 # Upload individual files
-hf_api.upload_file(
-    path_or_fileobj="./output/gith/toy.repos.csv.mt10.ts2/indexes.pkl",
-    path_in_repo="/output/gith/toy.repos.csv.mt10.ts2/indexes.pkl",
-    repo_id="fani-lab/OpeNTF-test",
-    repo_type="dataset"
-)
+# hf_api.upload_file(
+#     path_or_fileobj="./output/gith/toy.repos.csv.mt10.ts2/indexes.pkl",
+#     path_in_repo="/output/gith/toy.repos.csv.mt10.ts2/indexes.pkl",
+#     repo_id="fani-lab/OpeNTF-test",
+#     repo_type="dataset"
+# )
 
 
 # # Download a file from the hub
-if(hf_api.file_exists(repo_id="fani-lab/OpeNTF-test", repo_type="dataset", filename="teams.pkl")):
-    hf_api.hf_hub_download(repo_id="fani-lab/OpeNTF-test", repo_type="dataset", filename="output/gith/toy.repos.csv.mt10.ts2/indexes.pkl", local_dir='.')
-else:
-    print("File does not exist")
+# if(hf_api.file_exists(repo_id="fani-lab/OpeNTF-test", repo_type="dataset", filename="output/gith/toy.repos.csv.mt10.ts2/indexes.pkl")):
+#     hf_api.hf_hub_download(repo_id="fani-lab/OpeNTF-test", repo_type="dataset", filename="output/gith/toy.repos.csv.mt10.ts2/indexes.pkl", local_dir='.')
+# else:
+#     print("File does not exist")
