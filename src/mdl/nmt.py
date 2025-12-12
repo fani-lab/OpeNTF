31f137db39e4b3dbfe451f748a392967cc8defa6
Hossein Fani <hosseinfani@gmail.com>
2025-12-11 23:33:12 -0500

    (#340)(#281)

--- src/mdl/nmt.py
+++ src/mdl/nmt.py
@@ -1,4 +1,4 @@
-import pickle, subprocess, os, re, shlex, numpy as np, logging, sys, scipy
+import subprocess, os, re, shlex, numpy as np, logging, sys, scipy, shutil
 from omegaconf import OmegaConf
 log = logging.getLogger(__name__)
 
@@ -98,6 +98,8 @@ class Nmt(Ntf):
             onmt_train()
 
     def test(self, teamsvecs, splits, testcfg):
+        pd = opentf.install_import('pandas')
+        Y = teamsvecs['member'][splits['test']]
         for foldidx in splits['folds'].keys():
             onmtcfg = OmegaConf.load(f'{self.output}/f{foldidx}.config.yml')
             modelfiles = [f'{self.output}/{_}' for _ in os.listdir(self.output) if re.match(f'f{foldidx}._step_\d+.pt', _)]
@@ -108,7 +110,7 @@ class Nmt(Ntf):
                 cli_cmd = 'onmt_translate '
                 cli_cmd += f'-model {modelfile} '
                 cli_cmd += f'-src {self.output}/src-test.txt '
-                cli_cmd += f'-output {self.output}/f{foldidx}.test.e{step}.pred '
+                cli_cmd += f'-output {self.output}/f{foldidx}.test.e{step}.pred_ '
                 cli_cmd += f'-gpu {self.device.split(":")[1] if ":" in self.device else "0"} ' if 'cuda' in self.device else ''
                 cli_cmd += f'--min_length {onmtcfg.min_length} --max_length {onmtcfg.max_length} '
                 cli_cmd += f'--beam_size {onmtcfg.beam_size} --n_best {onmtcfg.n_best} '
@@ -122,66 +124,26 @@ class Nmt(Ntf):
                 # from onmt.translate.translator import build_translator
                 # translator = build_translator(opt_path_or_dict="config.yaml",  report_score=True)
                 # results = translator.translate(src=["Hello world!", "How are you?"],batch_size=2)
-    
-    #TODO: many code overlaps with ntf.evaluate ...
-    def evaluate(self, teamsvecs, splits, evalcfg):
-        pd = opentf.install_import('pandas')
-        import evl.metric as metric
-        fold_mean = pd.DataFrame()
-        mean_std = pd.DataFrame()
-        if evalcfg.per_instance: fold_mean_per_instance = pd.DataFrame()
-        Y = teamsvecs['member'][splits['test']]
-        for foldidx in splits['folds'].keys():
-            predfiles = [_ for _ in os.listdir(self.output) if re.match(f'f{foldidx}.test.e\d+.pred$', _)]
+            predfiles = [_ for _ in os.listdir(self.output) if re.match(f'f{foldidx}.test.e\d+.pred_$', _)]
             predfiles = sorted(predfiles, key=lambda f: int(re.search(r'\.e(\d+)\.', f).group(1)), reverse=True)
-            #per_epoch depends on "save_checkpoint_steps" param in nmt_config.yaml since it simply collect the checkpoint files
-            #so, per_epoch => per_checkpoints/steps
-            if not evalcfg.per_epoch: predfiles = predfiles[0]  # only the last step as the final model if no per_epoch
+            # per_epoch depends on "save_checkpoint_steps" param in nmt_config.yaml since it simply collect the checkpoint files
+            # so, per_epoch => per_checkpoints/steps
             for i, predfile in enumerate(predfiles):
                 df_pred = pd.read_csv(f'{self.output}/{predfile}', header=None)
-                #df_pred = pd.read_csv(f'{self.output}/tgt-test.txt', header=None) # for unit-test, y_ = y
-                #TODO: sparse?
-                Y_ = np.zeros((Y.shape[0], Y.shape[1]))
+                # df_pred = pd.read_csv(f'{self.output}/tgt-test.txt', header=None) # for unit-test, y_ = y
+                # TODO: sparse?
+                Y_ = Ntf.torch.zeros((Y.shape[0], Y.shape[1]))
                 for _ in range(Y.shape[0]):
                     predlist = (df_pred.iloc[_])[0]
                     predlist = re.sub(r'\baveryunlikelytoken\w*\b', '', predlist).strip().replace('<unk>', '').replace('<s>', '').replace('m', '').split()
-                    for pred in predlist: Y_[_, int(pred)] = 1/len(predlist)
+                    for pred in predlist: Y_[_, int(pred)] = 1 / len(set(predlist)) # seq2seq may repeat a token, here an expert
 
-                log.info(f'Evaluating predictions at {self.output}/{predfile} ... for {evalcfg.metrics}')
+                Y_ = opentf.topk_sparse(Ntf.torch, Y_, testcfg.topK) if (testcfg.topK and testcfg.topK < Y_.shape[1]) else Y_
+                torchfile = f'{self.output}/{predfile.replace(".pred_", ".pred")}'
+                Ntf.torch.save({'y_pred': Y_, 'uncertainty': None}, torchfile, pickle_protocol=4)
+                if i == 0: shutil.copy2(torchfile, f'{self.output}/f{foldidx}.test.pred') # the first in the reverse-sorted list is the last epoch, so we need a new file to be consistent with other models
 
-                df, df_mean = pd.DataFrame(), pd.DataFrame()
-                if evalcfg.metrics.trec:
-                    log.info(f'{evalcfg.metrics.trec} ...')
-                    df, df_mean = metric.calculate_metrics(Y, Y_, evalcfg.topK, evalcfg.per_instance, evalcfg.metrics.trec)
-
-                if 'aucroc' in evalcfg.metrics.other:
-                    log.info("['aucroc'] and curve values (fpr, tpr) ...")
-                    aucroc, fpr_tpr = metric.calculate_auc_roc(Y, Y_)
-                    if df_mean.empty: df_mean = pd.DataFrame(columns=['mean'])
-                    df_mean.loc['aucroc'] = aucroc
-                    with open(f'{self.output}/{predfile}.eval.roc.pkl', 'wb') as outfile: pickle.dump(fpr_tpr, outfile)
-
-                if (m := [m for m in evalcfg.metrics.other if 'skill_coverage' in m]):  # since this metric comes with topks str like 'skill_coverage_2,5,10'
-                    log.info(f'{m} ...')
-                    X = teamsvecs['skill'] if scipy.sparse.issparse(teamsvecs['skill']) else teamsvecs['original_skill']  # to accomodate dense emb vecs of skills
-                    X = X[splits['test']]
-                    #TODO: for absolute 0 all, it should be 0?
-                    df_skc, df_mean_skc = metric.calculate_skill_coverage(X, Y_, teamsvecs['skillcoverage'], evalcfg.per_instance, topks=m[0].replace('skill_coverage_', ''))
-                    if df.empty: df = df_skc
-                    else: df = pd.concat([df.reset_index(drop=True), df_skc.reset_index(drop=True)], axis=1)
-                    if df_mean.empty: df_mean = df_mean_skc
-                    else: df_mean = pd.concat([df_mean, df_mean_skc], axis=0)
-
-                if evalcfg.per_instance: df.to_csv(f'{self.output}/{predfile}.eval.instance.csv', float_format='%.5f', index=False)
-                log.info(f'Saving file per fold as {self.output}/{predfile}.eval.mean.csv')
-                df_mean.to_csv(f'{self.output}/{predfile}.eval.mean.csv')
-                if i == 0:  # non-epoch-based only, as there is different number of epochs for each fold model due to earlystopping
-                    fold_mean = pd.concat([fold_mean, df_mean], axis=1)
-                    if evalcfg.per_instance: fold_mean_per_instance = fold_mean_per_instance.add(df, fill_value=0)
-        mean_std['mean'] = fold_mean.mean(axis=1)
-        mean_std['std'] = fold_mean.std(axis=1)
-        log.info(f'Saving mean evaluation file over {len(splits["folds"])} folds as {self.output}/test.pred.eval.mean.csv')
-        mean_std.to_csv(f'{self.output}/test.pred.eval.mean.csv')
-        if evalcfg.per_instance: fold_mean_per_instance.truediv(len(splits['folds'].keys())).to_csv(f'{self.output}/test.pred.eval.instance.mean.csv', index=False)
-
+    def evaluate(self, teamsvecs, splits, evalcfg):
+        evalcfg.on_train=None # we cannot have on_train or on_valid for nmt
+        super().evaluate(teamsvecs, splits, evalcfg)
 
